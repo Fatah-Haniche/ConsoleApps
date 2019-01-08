bbc57c383c9a3f884a4b19bcc1d9cc17d60c9406
Fatah-Haniche <fhaniche@gmail.com>
2019-01-08 22:47:49 +0000

    Add project files.

---
+++ GidDev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GidDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
